b7e133803d0e944f70d5ec3a793e262ad6cea044
fpaniaguajavascript <fernando.paniagua.unity@gmail.com>
2024-04-28 09:22:42 +0200

    EnemigosListos avanzados

--- Assets/_GameObjects/Scripts/Swat.cs
+++ Assets/_GameObjects/Scripts/Swat.cs
@@ -7,21 +7,28 @@ using UnityEngine;
 public class Swat : MonoBehaviour
 {
     public Transform target;
-    public float speed;
+    public float walkSpeed;
+    public float runSpeed;
     public float fireDistance;
-    public float followDistance;
+    public float walkDistance;
+    public float runDistance;
     public float distance;
-    
-    
+
+
     void Update()
     {
         distance = (target.position - transform.position).magnitude;
         GetComponentInChildren<Animator>().SetFloat("Distance", distance);
         transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
-        if (distance < followDistance && distance > fireDistance)
+        if (distance < runDistance && distance > walkDistance)
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        } else if (distance < fireDistance)
+            transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
+        }
+        else if (distance < walkDistance && distance > fireDistance)
+        {
+            transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+        }
+        else if (distance < fireDistance)
         {
             //TODO: Fire
         }
