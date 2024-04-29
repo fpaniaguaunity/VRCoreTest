4e249997384ef8eba8b736726452633b6bc75c40
fpaniaguajavascript <fernando.paniagua.unity@gmail.com>
2024-04-29 11:12:01 +0200

    Panel de control - No funciona

---
+++ Assets/_GameObjects/Scripts/SceneManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject panel;
+    public InputActionReference inputActionReference;
+
+
+    private void OnEnable()
+    {
+        inputActionReference.action.performed += Activar;
+        inputActionReference.action.Enable();
+    }
+    private void OnDisable()
+    {
+        inputActionReference.action.performed -= Activar;
+        inputActionReference.action.Disable();
+    }
+    public void Activar(InputAction.CallbackContext obj)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+    
+}
