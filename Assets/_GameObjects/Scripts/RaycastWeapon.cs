5fbf2027840d7a515bddf65aece186a2c2ebea55
Fernando Paniagua Formación <fernando.paniagua.unity@gmail.com>
2024-04-30 19:18:01 +0200

    Extension

--- Assets/_GameObjects/Scripts/RaycastWeapon.cs
+++ Assets/_GameObjects/Scripts/RaycastWeapon.cs
@@ -6,19 +6,21 @@ public class RaycastWeapon : MonoBehaviour
 {
     public Transform raycastOrigin;
     public Material hitMaterial;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Fire(){
         Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out RaycastHit hitInfo);
+        
+        if (hitInfo.collider.CompareTag("Enemigo")){
+            //hacer lo que sea 
+        } else if (hitInfo.collider.CompareTag("Amigo")){
+            //Lo que sea
+        } else {
+            //Lo que sea
+        }
+
+        
+        //Parte del cambio de material
         Renderer renderer = hitInfo.collider.gameObject.GetComponent<Renderer>();
         if (renderer != null)
         {
